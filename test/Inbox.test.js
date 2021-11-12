b3c6a2df640b8711d63c93bca2b44c8e0cdde2a3
Leonard Flachs <leonard.flachs@gmail.com>
2021-11-12 20:05:50 +0100

    :rocket: add message contract call

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,7 +3,10 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const http = require("http");
 const Web3HttpProvider = require("web3-providers-http");
-const { interface, bytecode } = require("../compile");
+const contractFile = require("../compile");
+// Initialization
+const bytecode = contractFile.evm.bytecode.object;
+const abi = contractFile.abi;
 
 const provider = new Web3HttpProvider("http://localhost:7545");
 // const provider = ganache.provider()
@@ -17,17 +20,28 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(abi)
     .deploy({
       data: bytecode,
-      arguments: ["Hi there"],
+      arguments: ["test"],
     })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(inbox.options.address);
+    // console.log(inbox.options.address);
     assert.ok(inbox.options.address);
+  });
+
+  it("has the default message", async () => {
+    // console.log(inbox);
+    // const test = await inbox.methods
+    //   .setMessage("test")
+    //   .send({ from: accounts[0] });
+
+    const message = await inbox.methods.message().call({ from: accounts[0] });
+    console.log("message is", message);
+    // assert.equal(message, "Hi there");
   });
 });
